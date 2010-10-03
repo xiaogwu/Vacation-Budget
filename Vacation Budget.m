40a439e911c2e5be30ccb2b8f3f351ca40f9f482
Xiao G. Wu <xiaogwu@gmail.com>
2010-10-02 21:40:41 -0700

    Chapter 5: Vacation Budget fundementals

--- Vacation Budget.m
+++ Vacation Budget.m
@@ -3,8 +3,21 @@
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
-    // insert code here...
-    NSLog(@"Hello, World!");
+	//Variables
+	float exchangeRate = 1.2500;
+	double budget = 1000.00;
+	double euroCharge;
+	
+	NSLog(@"Starting budget is $%.2f", budget);
+	
+	budget = budget - 100;
+	NSLog(@"Spending $100 leaves $%.2f in the budget", budget);
+	
+	euroCharge = 100 * exchangeRate;
+	budget = budget - euroCharge;
+	NSLog(@"100 Euros is equal to $%.2f", euroCharge);
+	NSLog(@"Charging 100 Euros leaves $%.2f in the budget", budget);
+	
     [pool drain];
     return 0;
 }
