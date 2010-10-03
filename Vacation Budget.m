fc330a0a58422148db21173c911926386274f0a0
Xiao G. Wu <xiaogwu@gmail.com>
2010-10-02 22:21:17 -0700

    Chapter 5: Adding functions

--- Vacation Budget.m
+++ Vacation Budget.m
@@ -6,21 +6,33 @@ typedef struct {
 	double euroCharge;
 } budget;
 
+budget vacationBudget;
+
+//spendDollars function
+void spendDollars (double dollars) {
+	vacationBudget.budget = vacationBudget.budget - dollars;
+}
+
+//chargeEuro function
+void chargeEuros (double euros) {
+	vacationBudget.euroCharge = euros * vacationBudget.exchangeRate;
+	vacationBudget.budget = vacationBudget.budget - vacationBudget.euroCharge;
+}
+
 int main (int argc, const char * argv[]) {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 
 	//Variables
-	budget vacationBudget;
-	
 	vacationBudget.budget = 1000.00;
 	vacationBudget.exchangeRate = 1.2500;
+	double numberDollars = 100;
+	double numberEuros = 100;
 	NSLog(@"Starting budget is $%.2f", vacationBudget.budget);
 	
-	vacationBudget.budget = vacationBudget.budget - 100;
+	spendDollars(numberDollars);
 	NSLog(@"Spending $100 leaves $%.2f in the budget", vacationBudget.budget);
 	
-	vacationBudget.euroCharge = 100 * vacationBudget.exchangeRate;
-	vacationBudget.budget = vacationBudget.budget - vacationBudget.euroCharge;
+	chargeEuros(numberEuros);
 	NSLog(@"100 Euros is equal to $%.2f", vacationBudget.euroCharge);
 	NSLog(@"Charging 100 Euros leaves $%.2f in the budget", vacationBudget.budget);
 	
